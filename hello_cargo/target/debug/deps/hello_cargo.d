f7038c29bcb9da6081aedb7459a5cba360e9be47
squishybaker <grrtt.slmn@gmail.com>
2024-01-08 21:02:00 -0800

    general

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+X:\Users\grrtt\source\repos\rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+X:\Users\grrtt\source\repos\rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+X:\Users\grrtt\source\repos\rust\hello_cargo\target\debug\hello_cargo.exe: X:\Users\grrtt\source\repos\rust\hello_cargo\src\main.rs
