1c56e4a0336a2a2658205bac2601ea9716aedb8f
squishybaker <grrtt.slmn@gmail.com>
2024-01-09 07:31:55 -0800

    general

---
+++ hello_cargo/target/debug/deps/hello_cargo-380b3488f67e9a69.d
@@ -0,0 +1,5 @@
+X:\Users\grrtt\source\repos\rust\hello_cargo\target\debug\deps\libhello_cargo-380b3488f67e9a69.rmeta: src\main.rs
+
+X:\Users\grrtt\source\repos\rust\hello_cargo\target\debug\deps\hello_cargo-380b3488f67e9a69.d: src\main.rs
+
+src\main.rs:
