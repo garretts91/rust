d5461cb860f9f99e69022b9c69808417b0974cef
squishybaker <grrtt.slmn@gmail.com>
2024-01-25 16:54:26 -0800

    general

--- logic_program/target/debug/logic_program.d
+++ logic_program/target/debug/logic_program.d
@@ -1,1 +1,1 @@
-/home/spectre/Desktop/rust/rust/logic_program/target/debug/logic_program: /home/spectre/Desktop/rust/rust/logic_program/src/main.rs
+X:\Users\grrtt\source\repos\rust\logic_program\target\debug\logic_program.exe: X:\Users\grrtt\source\repos\rust\logic_program\src\main.rs
