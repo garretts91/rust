dc855741f24d94dc91e0a07fccdca738d40281e5
squishybaker <grrtt.slmn@gmail.com>
2024-01-31 14:20:08 -0800

    general

--- test_program/target/debug/test_program.d
+++ test_program/target/debug/test_program.d
@@ -1,1 +1,1 @@
-X:\Users\grrtt\source\repos\rust\test_program\target\debug\test_program.exe: X:\Users\grrtt\source\repos\rust\test_program\src\main.rs
+/home/spectre/Desktop/rust/rust/test_program/target/debug/test_program: /home/spectre/Desktop/rust/rust/test_program/src/main.rs
