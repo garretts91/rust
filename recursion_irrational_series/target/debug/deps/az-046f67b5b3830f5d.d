9f4f219b5b19e246d76255b985c7cd09ff2dc324
squishybaker <grrtt.slmn@gmail.com>
2024-02-07 13:38:15 -0800

    general

---
+++ recursion_irrational_series/target/debug/deps/az-046f67b5b3830f5d.d
@@ -0,0 +1,7 @@
+X:\Users\grrtt\source\repos\rust\recursion_irrational_series\target\debug\deps\libaz-046f67b5b3830f5d.rmeta: C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\az-1.2.1\src\lib.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\az-1.2.1\src\float.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\az-1.2.1\src\int.rs
+
+X:\Users\grrtt\source\repos\rust\recursion_irrational_series\target\debug\deps\az-046f67b5b3830f5d.d: C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\az-1.2.1\src\lib.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\az-1.2.1\src\float.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\az-1.2.1\src\int.rs
+
+C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\az-1.2.1\src\lib.rs:
+C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\az-1.2.1\src\float.rs:
+C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\az-1.2.1\src\int.rs:
---
+++ recursion_irrational_series/target/debug/deps/libc-7fd5fa185edf8859.d
@@ -0,0 +1,9 @@
+X:\Users\grrtt\source\repos\rust\recursion_irrational_series\target\debug\deps\liblibc-7fd5fa185edf8859.rmeta: C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\lib.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\macros.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\fixed_width_ints.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\windows\mod.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\windows\msvc\mod.rs
+
+X:\Users\grrtt\source\repos\rust\recursion_irrational_series\target\debug\deps\libc-7fd5fa185edf8859.d: C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\lib.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\macros.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\fixed_width_ints.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\windows\mod.rs C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\windows\msvc\mod.rs
+
+C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\lib.rs:
+C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\macros.rs:
+C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\fixed_width_ints.rs:
+C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\windows\mod.rs:
+C:\Users\grrtt\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\src\windows\msvc\mod.rs:
