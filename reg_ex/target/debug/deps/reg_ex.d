d946fd79c493088e3675e848bb3b1db5d7ea87a5
squishybaker <grrtt.slmn@gmail.com>
2024-02-28 19:05:38 -0800

    general

---
+++ reg_ex/target/debug/deps/reg_ex.d
@@ -0,0 +1,5 @@
+X:\Users\grrtt\source\repos\rust\reg_ex\target\debug\deps\reg_ex.exe: src\main.rs
+
+X:\Users\grrtt\source\repos\rust\reg_ex\target\debug\deps\reg_ex.d: src\main.rs
+
+src\main.rs:
---
+++ reg_ex/target/debug/reg_ex.d
@@ -0,0 +1,1 @@
+X:\Users\grrtt\source\repos\rust\reg_ex\target\debug\reg_ex.exe: X:\Users\grrtt\source\repos\rust\reg_ex\src\main.rs
